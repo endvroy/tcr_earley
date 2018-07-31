9973db32c9803394d95d2827b0e4da33d1248f82
Ruoyi Wang <endvroy@gmail.com>
2018-07-30 21:47:53 -0700

    normalize grammar can now be run separately

--- normalize_grammar.py
+++ normalize_grammar.py
@@ -1,4 +1,5 @@
 from normalize_antlr4 import make_builder
+import argparse
 
 
 def to_cnf(grammar_path, start):
@@ -8,3 +9,12 @@ def to_cnf(grammar_path, start):
     with open('tmp.g4', 'w') as f:
         f.write('grammar tmp;\n\n')
         f.write(s)
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description='Normalize ANTLR Grammar to CNF')
+    arg_parser.add_argument('grammar', help='path to grammar file')
+    arg_parser.add_argument('start', help='starting symbol')
+    args = arg_parser.parse_args()
+
+    to_cnf(args.grammar, args.start)
